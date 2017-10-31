6da65b61caf710a61cb95680d52a72a4e63c5740
Daniel Parks <daniel.parks@bbc.co.uk>
2017-10-31 11:22:09 +0000

    + DELPRKS-ADD-INDEXES: Add indexes to offers table

--- migration/001-offer.sql
+++ migration/001-offer.sql
@@ -10,7 +10,11 @@ CREATE TABLE offer
     available_to DATETIME NOT NULL,
     starting_price INT NOT NULL,
     currency VARCHAR(40) NOT NULL,
-    category VARCHAR(255) NOT NULL
+    category VARCHAR(255) NOT NULL,
+    status VARCHAR(40) NOT NULL
 );
 
 CREATE UNIQUE INDEX offer_id_uindex ON offer (id);
+CREATE INDEX offer_userid_index ON offer (user_id);
+CREATE INDEX offer_status_index ON offer (status);
+CREATE INDEX offer_category_index ON offer (category);
